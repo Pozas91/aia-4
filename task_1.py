aad94ecdbbacffe601bafe39b3a6867a940211fc
Nicolás Pozas García <pozas91@gmail.com>
2018-03-22 21:03:26 +0100

    First task and third task done!

--- task_1.py
+++ task_1.py
@@ -8,163 +8,188 @@ from operator import itemgetter
 import numpy as np
 import utils.text as ut
 
-########################################################################################################################
 
-tt = time()
+def task_1(texts_samples: list, texts_showed: int):
 
-print()
+    """
+    Método principal para ejecutar la tarea 1
+    :param texts_samples:
+        Textos para de los cuales queremos saber la cercanía
+    :param texts_showed:
+        Número de textos más afines a mostrar para los textos dados por el parámetro
+    :return:
+    """
 
-########################################################################################################################
+    # Comprobación de que los parámetros indicados deben ser los correctos
+    if not texts_samples:
+        raise ValueError('Debe haber al menos un texto de ejemplo.')
 
-t0 = time()
+    if texts_showed <= 0:
+        raise ValueError('Se debe mostrar al menos un texto similar por cada ejemplo dado.')
 
-# Categorias a cargar
-categories = [
-    'comp.graphics',
-    'comp.os.ms-windows.misc',
-    'comp.sys.ibm.pc.hardware',
-    'comp.sys.mac.hardware',
-    'comp.windows.x',
-    'sci.space'
-]
+    ####################################################################################################################
 
-print('Cargando el dataset de twenty_newsgroups para las siguientes categorias:')
-print(categories)
+    t0 = time()
 
-# Cogemos todos los grupos de post, de las categorias dadas, de forma aleatoria eliminando cabeces, pies y citas.
-dataset = fetch_20newsgroups(
-    subset='all', categories=categories, shuffle=True, random_state=42, remove=('headers', 'footers', 'quotes')
-)
+    # Categorias a cargar
+    categories = [
+        'comp.graphics',
+        'comp.os.ms-windows.misc',
+        'comp.sys.ibm.pc.hardware',
+        'comp.sys.mac.hardware',
+        'comp.windows.x',
+        'sci.space'
+    ]
 
-print('Terminado en %fs' % (time() - t0))
-print()
+    print('Cargando el dataset de twenty_newsgroups para las siguientes categorias... {0}'.format(categories))
 
-########################################################################################################################
+    # Cogemos todos los grupos de post, de las categorias dadas, de forma aleatoria eliminando cabeces, pies y citas.
+    dataset = fetch_20newsgroups(
+        subset='all', categories=categories, shuffle=True, random_state=42, remove=('headers', 'footers', 'quotes')
+    )
 
-t0 = time()
-print('Vectorizamos el conjunto de palabras')
-
-# Valor decimal [0.0, 1.0], cuando se construya el vocabulario se ignorarán los terminos que tengan mayor frecuencia
-# documental a este umbral (Usado mayormente para evitar las stop-words, o palabras que aparezcan demasiado y no sean
-# relevantes). Cuando se introduce un número entero referencia a la cantidad de palabras y no a la frecuencia
-# documental.
-max_df = 0.5
-
-# Valor decimal [0.0, 1.0], cuando se construya se ignorarán los términos que tengan menor frecuencia umbral a este
-# umbral (Usado mayormente para evitar palabras muy poco usadas, o demasiado específicas del documento). Cuando se
-# introduce un número entero referencia a la cantidad de palabras y no a la frecuencia documental
-min_df = 2
-
-# Idioma de las stop-words
-stop_words = 'english'
-
-# Activa la frencuencia de documentación inversa en los pesos de las palabras.
-use_idf = True
-
-# Creamos un vector tfidf con las siguientes características
-vectorizer = TfidfVectorizer(
-    max_df=max_df, min_df=min_df, stop_words=stop_words, use_idf=use_idf, tokenizer=ut.stemmer_tokenizer
-)
-
-# Entrenamos el vector y transformamos los datos en un vector numérico
-x_train = vectorizer.fit_transform(dataset.data)
-
-print('Terminado en %fs' % (time() - t0))
-print('Nº de ejemplos: %d, Nº de características: %d' % x_train.shape)
-print()
-
-########################################################################################################################
-
-t0 = time()
-test = ['I\'m working with Windows for the IA subject.', 'New nvidia GTX2048 in shops, for only 10000$']
-print('Vectorizamos el conjunto de prueba')
-
-# Pasamos el conjunto de test a un vector numérico con la información previa entrenada, de esta forma tendremos las
-# posiciones de las palabras de la frase dada
-x_test = vectorizer.transform(test)
-
-print('Terminado en %fs' % (time() - t0))
-print('Nº de ejemplos: %d, Nº de características: %d' % x_test.shape)
-print()
-
-########################################################################################################################
-
-t0 = time()
-print('Realizamos el algoritmo K-media, para organizar por clústeres')
-
-# Ahora realizamos el algoritmo K-medias para optimizar la ejecución del problema y dar un conjunto de datos donde se
-# clasifique nuestro conjunto de entrenamiento. Se han probado diferentes tamaños de clústeres:
-# 10 --> 141s
-# 20 --> 116s
-# 30 --> 149s
-# 40 --> 128s
-# 50 --> 343s
-# dando como mejor resultado y más rápido 20 clústeres.
-k_means = KMeans(n_clusters=20)
-k_means.fit(x_train)
-
-print('Terminado en %fs' % (time() - t0))
-print('Nº de centros: %d, Nº de características: %d' % k_means.cluster_centers_.shape)
-print()
-
-########################################################################################################################
-
-t0 = time()
-print('Clasificamos el centro donde quedaría nuestro conjunto de prueba')
-
-# Sacamos el centro del conjunto de prueba
-prediction = k_means.predict(x_test)
-
-print('Terminado en %fs' % (time() - t0))
-print('El conjunto de prueba ha quedado clasificado en los centros: %s' % prediction)
-print()
-
-########################################################################################################################
-
-t0 = time()
-
-# Textos que vamos a mostrar
-texts_showed = 3
-
-print('Sacamos los %d posts más parecidos a los mencionados por el usuario.' % texts_showed)
-
-posts = dict()
-for center_index, center in enumerate(prediction):
-    for label_index in np.where(k_means.labels_ == center)[0]:
-
-        # Obtenemos la similitud entre la frase del conjunto de prueba, y la del conjunto de entrenamiento
-        sim = ut.similarity_words(x_test[center_index].toarray()[0], x_train[label_index].toarray()[0])
-
-        if center_index in posts:
-            values = posts.get(center_index)
-            values.append((label_index, sim))
-            posts.update({center_index: values})
-        else:
-            posts.update({center_index: [(label_index, sim)]})
-
-# Por cada texto de prueba
-for center in posts:
-
-    # Sacamos el cluster al que pertenece
-    cluster = prediction[center]
-
-    # Sacamos las palabras ordenadas de mayor a menor de forma eficiente
-    labels = sorted(posts.get(center), key=itemgetter(1), reverse=True)
-
-    print('Para el texto de prueba: ')
-    print(test[center])
+    print('Terminado en {0:.2f}s'.format(time() - t0))
     print()
 
-    print('Se han encontrado las siguientes textos más similares:')
+    ####################################################################################################################
 
-    # Nos quedamos con las x primeros y los mostramos
-    for key, sim in labels[:texts_showed]:
-        print(dataset.data[key])
-        print('-------------------------------------------------------------------------------------------------------')
+    t0 = time()
+    print('Vectorizamos el conjunto de palabras...', end=' ')
 
-print('Terminado en %fs' % (time() - t0))
-print()
+    # Valor decimal [0.0, 1.0], cuando se construya el vocabulario se ignorarán los terminos que tengan mayor
+    # frecuencia documental a este umbral (Usado mayormente para evitar las stop-words, o palabras que aparezcan
+    # demasiado y no sean relevantes). Cuando se introduce un número entero referencia a la cantidad de palabras y no
+    #  a la frecuencia documental.
+    max_df = 0.5
 
-########################################################################################################################
+    # Valor decimal [0.0, 1.0], cuando se construya se ignorarán los términos que tengan menor frecuencia umbral a este
+    # umbral (Usado mayormente para evitar palabras muy poco usadas, o demasiado específicas del documento). Cuando se
+    # introduce un número entero referencia a la cantidad de palabras y no a la frecuencia documental
+    min_df = 2
 
-print('La ejecución total del programa ha sido de %fs' % (time() - tt))
+    # Idioma de las stop-words
+    stop_words = 'english'
+
+    # Activa la frencuencia de documentación inversa en los pesos de las palabras.
+    use_idf = True
+
+    # Creamos un vector tfidf con las siguientes características
+    vectorizer = TfidfVectorizer(
+        max_df=max_df, min_df=min_df, stop_words=stop_words, use_idf=use_idf, tokenizer=ut.stemmer_tokenizer
+    )
+
+    # Entrenamos el vector y transformamos los datos en un vector numérico
+    x_train = vectorizer.fit_transform(dataset.data)
+
+    print('terminado en {0:.2f}s'.format(time() - t0))
+    print('Nº de ejemplos: {0:d}, Nº de características: {1:d}'.format(x_train.shape[0], x_train.shape[1]))
+    print()
+
+    ####################################################################################################################
+
+    t0 = time()
+    print('Vectorizamos el conjunto de prueba...', end=' ')
+
+    # Pasamos el conjunto de test a un vector numérico con la información previa entrenada, de esta forma tendremos las
+    # posiciones de las palabras de la frase dada
+    x_test = vectorizer.transform(texts_samples)
+
+    print('terminado en {0:.2f}s'.format(time() - t0))
+    print('Nº de ejemplos: {0:d}, Nº de características: {1:d}'.format(x_test.shape[0], x_test.shape[1]))
+    print()
+
+    ####################################################################################################################
+
+    t0 = time()
+    print('Realizamos el algoritmo K-media, para organizar por clústeres...', end=' ')
+
+    # Ahora realizamos el algoritmo K-medias para optimizar la ejecución del problema y dar un conjunto de datos donde se
+    # clasifique nuestro conjunto de entrenamiento. Se han probado diferentes tamaños de clústeres:
+    # 10 --> 141s
+    # 20 --> 116s
+    # 30 --> 149s
+    # 40 --> 128s
+    # 50 --> 343s
+    # dando como mejor resultado y más rápido 20 clústeres.
+    k_means = KMeans(n_clusters=20)
+    k_means.fit(x_train)
+
+    print('terminado en {0:.2f}s'.format(time() - t0))
+    print('Nº de ejemplos: {0:d}, Nº de características: {1:d}'.format(
+        k_means.cluster_centers_.shape[0], k_means.cluster_centers_.shape[1]
+    ))
+    print()
+
+    ####################################################################################################################
+
+    t0 = time()
+    print('Clasificamos el centro donde quedaría nuestro conjunto de prueba...', end=' ')
+
+    # Sacamos el centro del conjunto de prueba
+    prediction = k_means.predict(x_test)
+
+    print('terminado en {0:.2f}s'.format(time() - t0))
+    print('El conjunto de prueba ha quedado clasificado en los centros: {0}'.format(prediction))
+    print()
+
+    ####################################################################################################################
+
+    t0 = time()
+
+    print('Sacamos los {0:d} posts más parecidos a los mencionados por el usuario.'.format(texts_showed))
+
+    posts = dict()
+
+    for center_index, center in enumerate(prediction):
+        for label_index in np.where(k_means.labels_ == center)[0]:
+
+            # Obtenemos la similitud entre la frase del conjunto de prueba, y la del conjunto de entrenamiento
+            sim = ut.similarity_words(x_test[center_index].toarray()[0], x_train[label_index].toarray()[0])
+
+            if center_index in posts:
+                values = posts.get(center_index)
+                values.append((label_index, sim))
+                posts.update({center_index: values})
+            else:
+                posts.update({center_index: [(label_index, sim)]})
+
+    # Por cada texto de prueba
+    for center in posts:
+
+        # Sacamos el cluster al que pertenece
+        cluster = prediction[center]
+
+        # Sacamos las palabras ordenadas de mayor a menor de forma eficiente
+        labels = sorted(posts.get(center), key=itemgetter(1), reverse=True)
+
+        print('Para el texto de prueba: ')
+        print(texts_samples[center])
+        print()
+
+        print('Se han encontrado las siguientes textos más similares:')
+
+        # Nos quedamos con las x primeros y los mostramos
+        for key, sim in labels[:texts_showed]:
+
+            print('\n {0}'.format(dataset.data[key]))
+
+            # Añadimos una línea separatoria entre los documentos
+            print('\n' + '-' * 50)
+
+    print('Terminado en {0:.2f}s'.format(time() - t0))
+    print()
+
+
+if __name__ == '__main__':
+    print('Iniciando la ejecución del programa principal...', end='\n\n')
+
+    t0 = time()
+
+    samples = [
+        'I\'m working with Windows for the IA subject.',
+        'New nvidia GTX2048 in shops, for only 10000$'
+    ]
+
+    task_1(texts_samples=samples, texts_showed=3)
+
+    print('La ejecución total del programa ha sido de {0:.2f}s'.format(time() - t0))
--- task_3.py
+++ task_3.py
@@ -1,118 +1,159 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
 
 import pandas as pd
+import utils.text as ut
 import os
 from time import time
 
-########################################################################################################################
 
-tt = time()
-print()
+def task_3(reviews_samples: list, n_jobs: int, training_taken=200):
 
-########################################################################################################################
+    """
+    Método principal para ejecutar la tarea 3
+    :param reviews_samples:
+        Opiniones de ejemplo que queremos clasificar de acuerdo al sentimiento que transmiten
+    :param n_jobs:
+        Nº de procesadores usados para ejecutar el problema
+    :param training_taken:
+        Nº de datos de entrenamiento cogidos para entrenar al modelo
+    :return:
+    """
 
-t0 = time()
+    # Comprobación de que los parámetros indicados deben ser los correctos
+    if not reviews_samples:
+        raise ValueError('Debe haber al menos una opinión de ejemplo.')
 
-print('Cargando y pasando datos desde el csv...', end=' ')
+    if n_jobs <= 0 and n_jobs is not -1:
+        raise ValueError('El argumento n_jobs debe ser o -1 para trabajar con todos los núcleos del sistema, '
+                         'o el nº de núcleos indicado')
 
-movie_data = pd.read_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets/movie_data.csv'))
+    # Si el número indicado es menor a 0
+    # (Si es mayor al número total de películas que tenemos, simplemente cogerá las máximas posibles)
+    if training_taken <= 0:
+        raise ValueError('El argumento de valores de entrenamiento tomados debe ser un número mayor a 1.')
 
-x = list()
-y = list()
-target_names = ['Negativo', 'Positivo']
+    ####################################################################################################################
 
-for values in movie_data.values:
-    review, sentiment = values
-    x.append(review)
-    y.append(sentiment)
+    t0 = time()
 
-print('terminado en {0:.2f}s'.format(time() - t0))
-print()
+    print('Cargando y pasando datos desde el csv...', end=' ')
 
-########################################################################################################################
+    # Cargamos los datos con panda desde el CSV
+    movie_data = pd.read_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets/movie_data.csv'))
 
-t0 = time()
+    x = list()
+    y = list()
 
-print('Sacamos por validación cruzada el conjunto de entrenamiento y el de pruebas...', end=' ')
+    # Indicamos los tipos de sentimientos que tenemos
+    target_names = ['Negativo', 'Positivo']
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
-x_sample = ['Iron man movie it\'s fantastic.', 'The movie is a little bored.']
+    # Cogemos solo el número de datos indicado en la cabecera del método
+    for values in movie_data.values[:training_taken]:
+        review, sentiment = values
+        x.append(review)
+        y.append(sentiment)
 
-print('terminado en {0:.2f}s'.format(time() - t0))
-print()
-
-########################################################################################################################
-
-t0 = time()
-
-print('Creamos el vectorizador con las opiniones de las películas...', end=' ')
-
-vectorizer_parameters = {
-    'ngram_range': [(1, 3)],
-    'stop_words': ['english'],
-    'smooth_idf': [True, False],
-    'use_idf': [True, False],
-    'sublinear_tf': [True, False],
-    'binary': [True, False]
-}
-
-vectorizer = TfidfVectorizer(ngram_range=(1, 3), stop_words='english')
-
-x_vectorizer_train = vectorizer.fit_transform(x_train)
-x_vectorizer_test = vectorizer.transform(x_test)
-x_vectorizer_sample = vectorizer.transform(x_sample)
-
-print('terminado en {0:.2f}s'.format(time() - t0))
-print()
-
-########################################################################################################################
-
-t0 = time()
-
-print('Entrenamos las opiniones con el Multinomial de Naive Bayes...', end=' ')
-
-mnb_parameters = {
-    'alpha': [0.1, 0.25, 0.5, 0.75, 1.0]
-}
-
-# Creamos el GridSearch para el MultinomialNB
-gs_mnb = GridSearchCV(MultinomialNB(), mnb_parameters)
-
-# Lo entrenamos con los vectores
-gs_mnb.fit(x_vectorizer_train, y_train)
-
-print('terminado en {0:.2f}s'.format(time() - t0))
-print()
-
-########################################################################################################################
-
-t0 = time()
-
-print('Clasificamos y mostramos el rendimiento del Multinomial de Naive Bayes')
-
-# Clasificamos los vectores del conjunto de prueba
-prediction = gs_mnb.predict(x_vectorizer_sample)
-
-# Clasificamos los comentarios de ejemplo introducidos
-for i, predict in enumerate(prediction):
-    print('El comentario:')
-    print('\t {0}'.format(x_sample[i]))
-    print('\t Es {0}'.format(target_names[predict]))
+    print('terminado en {0:.2f}s'.format(time() - t0))
     print()
 
-# Comprobamos el rendimiento obtenido
-score_gs_mnb = gs_mnb.score(x_vectorizer_test, y_test)
+    ####################################################################################################################
 
-print('Los datos del clasificador son:')
-print('\t El rendimiento con el conjunto entrenado es del {0:.2f}%'.format(score_gs_mnb * 100))
-print('\t Los mejores parámetros seleccionados son: {0}'.format(gs_mnb.best_params_))
+    t0 = time()
 
-print('Terminado en {0:.2f}s'.format(time() - t0))
-print()
+    print('Preparando datos para el ejemplo...', end=' ')
 
-########################################################################################################################
+    x_train, y_train, x_sample = x, y, reviews_samples
 
-print('La ejecución total del programa ha sido de {0:.2f}s'.format(time() - tt))
+    print('terminado en {0:.2f}s'.format(time() - t0))
+    print()
+
+    ####################################################################################################################
+
+    t0 = time()
+
+    print('Creamos un Pipeline con el vectorizador y el clasificador...', end=' ')
+
+    pipeline = Pipeline([
+        ('tfidf', TfidfVectorizer(tokenizer=ut.stemmer_tokenizer, stop_words='english')),
+        ('multinomial', MultinomialNB())
+    ])
+
+    print('terminado en {0:.2f}s'.format(time() - t0))
+    print()
+
+    ####################################################################################################################
+
+    t0 = time()
+
+    print('Entrenamos el Pipeline con los datos de las opiniones de las películas...', end=' ')
+
+    parameters = {
+        'tfidf__ngram_range': [(1, 3)],
+        'tfidf__smooth_idf': [True, False],
+        'tfidf__use_idf': [True, False],
+        'tfidf__sublinear_tf': [True, False],
+        'tfidf__binary': [True, False],
+        'multinomial__alpha': [0.1, 0.25, 0.5, 0.75, 1.0]
+    }
+
+    # Creamos el GridSearch para el Pipeline seleccionado
+    gs_pipeline = GridSearchCV(pipeline, parameters, n_jobs=n_jobs)
+
+    # Lo entrenamos con los vectores
+    gs_pipeline.fit(x_train, y_train)
+
+    print('terminado en {0:.2f}s'.format(time() - t0))
+    print()
+
+    ####################################################################################################################
+
+    t0 = time()
+
+    print('Clasificamos y mostramos el rendimiento del pipeline')
+
+    # Clasificamos los vectores del conjunto de prueba
+    prediction = gs_pipeline.predict(x_sample)
+
+    # Clasificamos los comentarios de ejemplo introducidos
+    for i, predict in enumerate(prediction):
+        print('El comentario:')
+        print('\t {0}'.format(x_sample[i]))
+        print('\t Es {0}'.format(target_names[predict]))
+        print()
+
+    # Mostramos los datos del clasificador
+    print('Los datos del clasificador son:')
+
+    # Este mejor rendimiento se obtiene por medio de una validación cruzada, por tanto, no es necesario que la hagamos
+    # explícitamente
+    print('\t El mejor rendimiento obtenido con el conjunto entrenado es del: \n\t\t {0:.2f}%'.format(
+        gs_pipeline.best_score_ * 100
+    ))
+    print('\t Los mejores parámetros seleccionados son: \n\t\t {0}'.format(
+        gs_pipeline.best_params_
+    ))
+
+    print('Terminado en {0:.2f}s'.format(time() - t0))
+    print()
+
+
+if __name__ == '__main__':
+
+    print('Iniciando la ejecución del programa principal...', end='\n\n')
+
+    t0 = time()
+
+    samples = [
+        'Iron man movie it\'s fantastic.',
+        'The movie is a little bored.',
+        'It\'s a incredible film, I love it',
+        'The movie is not bad for the low budget it has.',
+        'In the beginning the movie was bored, but when Brad Pit kill zombies, it was amazing.'
+    ]
+
+    task_3(reviews_samples=samples, n_jobs=-1, training_taken=300)
+
+    print('La ejecución total del programa ha sido de {0:.2f}s'.format(time() - t0))
